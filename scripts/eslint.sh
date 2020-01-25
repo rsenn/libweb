72d5a4cda23e731522153d3cd447f4459dc1b5a6
Roman <roman.l.senn@gmail.com>
2020-01-25 01:14:37 +0100

    ...

--- scripts/eslint.sh
+++ scripts/eslint.sh
@@ -17,12 +17,14 @@ var_dump ()
 
 [ $# -gt 0 ] || set -- $(find components pages lib stores -name "*.js")
 
-
+[ -e node_modules/.bin/eslint ] && ESLINT=node_modules/.bin/eslint
 
 for FILE; do
   echo "$FILE" 1>&2
-  ${ESLINT:-eslint} --no-color --fix "$FILE" 2>&1 | tee eslint.tmp | sed "s,^\s*\([0-9]\+\):\([0-9]\+\)\s\+\([^ ]\+\) \+\(.*[^ ]\)\s\s*\([^\s]\+\)$,$1|\1|\3|\4|\5,"  \
-    | {
+  (set -x
+  ${ESLINT:-eslint} --no-color --fix "$FILE" 2>&1 
+  ) | tee eslint.tmp | #| sed "s,^\s*\([0-9]\+\):\([0-9]\+\)\s\+\([^ ]\+\) \+\(.*[^ ]\)\s\s*\([^\s]\+\)$,$1|\1|\3|\4|\5,"  \
+     {
     IFS="|"
     while read -r _FILE LINE WHAT MESSAGE RULE; do
       echo $FILE $LINE $WHAT $MESSAGE $RULE
