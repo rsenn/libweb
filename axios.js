4924942d09febf2037b517881b9e3eb3521d28dc
Roman <roman.l.senn@gmail.com>
2020-03-17 19:05:38 +0100

    ...

--- axios.js
+++ axios.js
@@ -1,5 +1,5 @@
-const axios = require("axios");
-const Util = require("./util.js");
+import axios from "axios";
+import Util from "./util.js";
 
 const httpClient = (() => {
   var client = axios.create({ withCredentials: true });
@@ -20,7 +20,7 @@ const httpClient = (() => {
   );
 
   let request = (fn, name = "call") =>
-    async function() {
+    (async function() {
       let args = [...arguments];
 
       if(typeof args[0] == "string" && args[0].startsWith("/")) {
@@ -29,7 +29,7 @@ const httpClient = (() => {
       //  console.error(`axios ${name}:`, args);
 
       return await fn.apply(client, args);
-    };
+    });
 
   let ret = request(client);
 
@@ -42,8 +42,7 @@ const httpClient = (() => {
 
 if(global.window) window.axios = httpClient;
 
-module.exports = httpClient;
-
-module.exports.default = httpClient;
-module.exports.axios = httpClient;
-module.exports.httpClient = httpClient;
+export default httpClient;
+export {httpClient as default};
+export {httpClient as axios};
+export {httpClient};
