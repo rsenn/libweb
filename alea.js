78c2a812eea84671590172fabe1daa7749aa2b7a
Roman <roman.l.senn@gmail.com>
2020-03-18 21:13:58 +0100

    ...

--- alea.js
+++ alea.js
@@ -1,19 +1,18 @@
-(function (root, factory) {
-  if (typeof exports === 'object') {
-      module.exports = factory();
-  } else if (typeof define === 'function' && define.amd) {
-      define(factory);
+(function(root, factory) {
+  if(typeof exports === 'object') {
+    module.exports = factory();
+  } else if(typeof define === 'function' && define.amd) {
+    define(factory);
   } else {
-      root.Alea = factory();
+    root.Alea = factory();
   }
-}(this, function () {
-
+})(this, function() {
   'use strict';
 
   // From http://baagoe.com/en/RandomMusings/javascript/
 
   // importState to sync generator states
-  Alea.importState = function(i){
+  Alea.importState = function(i) {
     var random = new Alea();
     random.importState(i);
     return random;
@@ -29,25 +28,25 @@
       var s2 = 0;
       var c = 1;
 
-      if (args.length == 0) {
-        args = [+new Date];
+      if(args.length == 0) {
+        args = [+new Date()];
       }
       var mash = Mash();
       s0 = mash(' ');
       s1 = mash(' ');
       s2 = mash(' ');
 
-      for (var i = 0; i < args.length; i++) {
+      for(var i = 0; i < args.length; i++) {
         s0 -= mash(args[i]);
-        if (s0 < 0) {
+        if(s0 < 0) {
           s0 += 1;
         }
         s1 -= mash(args[i]);
-        if (s1 < 0) {
+        if(s1 < 0) {
           s1 += 1;
         }
         s2 -= mash(args[i]);
-        if (s2 < 0) {
+        if(s2 < 0) {
           s2 += 1;
         }
       }
@@ -57,32 +56,30 @@
         var t = 2091639 * s0 + c * 2.3283064365386963e-10; // 2^-32
         s0 = s1;
         s1 = s2;
-        return s2 = t - (c = t | 0);
+        return (s2 = t - (c = t | 0));
       };
       random.uint32 = function() {
         return random() * 0x100000000; // 2^32
       };
       random.fract53 = function() {
-        return random() + 
-          (random() * 0x200000 | 0) * 1.1102230246251565e-16; // 2^-53
+        return random() + ((random() * 0x200000) | 0) * 1.1102230246251565e-16; // 2^-53
       };
       random.version = 'Alea 0.9';
       random.args = args;
 
       // my own additions to sync state between two generators
-      random.exportState = function(){
+      random.exportState = function() {
         return [s0, s1, s2, c];
       };
-      random.importState = function(i){
+      random.importState = function(i) {
         s0 = +i[0] || 0;
         s1 = +i[1] || 0;
         s2 = +i[2] || 0;
         c = +i[3] || 0;
       };
- 
-      return random;
 
-    } (Array.prototype.slice.call(arguments)));
+      return random;
+    })(Array.prototype.slice.call(arguments));
   }
 
   function Mash() {
@@ -90,7 +87,7 @@
 
     var mash = function(data) {
       data = data.toString();
-      for (var i = 0; i < data.length; i++) {
+      for(var i = 0; i < data.length; i++) {
         n += data.charCodeAt(i);
         var h = 0.02519603282416938 * n;
         n = h >>> 0;
@@ -106,4 +103,4 @@
     mash.version = 'Mash 0.9';
     return mash;
   }
-}));
+});
