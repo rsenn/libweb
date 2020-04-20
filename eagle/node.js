a956e720d20e9ab366335b482003ed09766f0db4
Roman <roman.l.senn@gmail.com>
2020-04-20 06:36:29 +0200

    ...

--- eagle/node.js
+++ eagle/node.js
@@ -1,14 +1,13 @@
 import { EaglePath, EagleRef, EagleReference } from "./locator.js";
-//import {  makeEagleElement } from "./element.js";
+
 import Util from "../util.js";
 import deep from "../deep.js";
 import { lazyMembers, lazyMap } from "../lazyInitializer.js";
 import { text, inspect, EagleInterface } from "./common.js";
-//import { EagleNodeList } from "./nodeList.js";
+
 import { makeEagleNodeMap } from "./nodeMap.js";
 
 export const makeEagleNode = (owner, ref, ctor) => {
-  /// console.log(`makeEagleNode(`,owner,', ',ref,', ',ctor,`)`);
   let e = new ctor(owner, ref);
   return e;
 };
@@ -18,54 +17,39 @@ export class EagleNode extends EagleInterface {
 
   constructor(owner, ref) {
     super(owner);
-    /*  if(d !== null && d.ref !== undefined && d.ref.root)
-      d = d.ref.root;
-*/
     ref = ref instanceof EagleReference ? ref : EagleRef(owner.ref.root, ref);
-    //let root = 'ref' in d ? d.ref.root : 'root' in d ? d.root : d;
     Util.define(this, "ref", ref);
   }
 
   get path() {
     return this.ref.path;
   }
+
   get root() {
     let root = this.ref.root;
-    /*
-       while(root.ref !== undefined && root.ref.root)
-         root = root.ref.root;*/
-
     return root;
   }
 
   get document() {
     let doc = this.owner;
-
     while(doc.owner !== undefined && doc.xml === undefined) doc = doc.owner;
-
     return doc;
   }
 
   get chain() {
     let doc = this.owner;
     let ret = [];
-
     while(doc !== undefined) {
       ret.push(doc);
       doc = doc.owner;
     }
-
     return ret;
   }
 
   getDocument() {
     let l = this.path.clone();
     let d = this.owner;
-
-    if(!(d instanceof EagleDocument) && this.path.length) {
-      while(!(d instanceof EagleDocument)) d = d[l.shift()];
-    }
-
+    if(!(d instanceof EagleDocument) && this.path.length) while(!(d instanceof EagleDocument)) d = d[l.shift()];
     return d;
   }
 
@@ -75,16 +59,13 @@ export class EagleNode extends EagleInterface {
   }
 
   get node() {
-    //  let root = this.root !== null && this.root.ref !== undefined && this.root.ref.root || this.root;
     let node = this.path.apply(this.root.raw);
-
     return node;
   }
 
   get raw() {
     return this.ref.dereference();
     let ret = {};
-
     if(this.xml && this.xml[0]) {
       ret = this.xml[0];
     } else {
@@ -93,11 +74,8 @@ export class EagleNode extends EagleInterface {
       if(this.context) ret.text = this.text;
       let children = [];
       if(this.children && "map" in this.children) children = this.children.map(child => child.raw || child.text).filter(child => child !== undefined);
-      /* if(children.length > 0) */ ret.children = children;
-    } /*else {
-      throw new Error("Cannot get raw");
-    }*/
-
+      ret.children = children;
+    }
     return ret;
   }
 
@@ -126,7 +104,6 @@ export class EagleNode extends EagleInterface {
 
   initCache(ctor = this.childConstructor) {
     let fields = this.cacheFields();
-    //console.log(`initCache(${Util.fnName(ctor)}) this:`, Util.className(this));
 
     if(fields) {
       Util.define(this, "cache", {});
@@ -139,14 +116,10 @@ export class EagleNode extends EagleInterface {
 
       for(let [value, path] of deep.iterate(ref.dereference(), v => v && fields.indexOf(v.tagName) != -1)) {
         const key = value.tagName;
-        // console.log(`cache list ${key}:`, path);
-
         lazy[key] = () => makeEagleNode(this, ref.down(...path), ctor);
-
         lists[key] = () => lazy[key]().children;
 
-        if(["sheets", "connects"].indexOf(key) != -1) maps[key] = lists[key];
-        else maps[key] = () => makeEagleNodeMap(lazy[key]().children, key == "instances" ? "part" : key == "layers" ? "number" : "name");
+        maps[key] = ["sheets", "connects"].indexOf(key) != -1 ? lists[key] : () => makeEagleNodeMap(lazy[key]().children, key == "instances" ? "part" : key == "layers" ? "number" : "name");
       }
       lazyMembers(this.lists, lists);
       lazyMembers(this.cache, lazy);
@@ -174,10 +147,9 @@ export class EagleNode extends EagleInterface {
   }
 
   get(name, pred, attr = "name", transform = (o, l) => makeEagleElement(this, l)) {
-    //  if(this.cache[name]) return this.cache[name];
     let i = name == "library" ? "libraries" : name + "s";
     let p = this[i];
-    let children = p; //||this.document[i].children);
+    let children = p;
     if(typeof attr != "function") {
       const attrName = "" + attr;
       attr = e => e.attributes[attrName];
@@ -195,21 +167,7 @@ export class EagleNode extends EagleInterface {
       function() {
         return [...arguments];
       };
-
     for(let [v, p, o] of deep.iterate(this.ref.dereference(), e => e.tagName === name)) yield transform(v, p, o);
-
-    //     name = name == "library" ? "libraries" : name + "s";
-    /* let a = this.cache[name + "s"];
-
-    if(a !== undefined) {
-      //   return EagleNodeList(a.ref, 'children');
-
-      for(let e of a.children) yield transform(e, e.name);
-
-      return;
-    }*/
-
-    //for(let [v, l, p] in this.findAll(e => e.tagName === name)) yield transform(v, l, p);
   }
 
   getMap(entity) {
@@ -219,17 +177,6 @@ export class EagleNode extends EagleInterface {
   }
 
   getByName(element, name, attr = "name", t = ([v, l, d]) => makeEagleNode(d, this.ref.down(...l), this.childConstructor)) {
-    /*if(this.cache[element+'s']) {
-  console.log(`this.cache[${element+'s'}]:`,this.cache[element+'s']);
-
-  for(let i = 0; i < this.cache[element+'s'].length; i++)  {
-    const obj = this.cache[element+'s'][i];
-    if(obj[attr] == name)
-      return obj;
-  }
-
-}*/
-
     for(let [v, l, d] of this.iterator([], it => it)) {
       if(typeof v == "object" && "tagName" in v && "attributes" in v && attr in v.attributes) {
         if(v.tagName == element && v.attributes[attr] == name) return t([v, l, d]);
@@ -280,13 +227,9 @@ export class EagleNode extends EagleInterface {
     let numChildren = this.raw.children && this.raw.children.length;
     if(numChildren == 0) attrs += " /";
     let ret = `${Util.className(this)}`;
-
     if(this.tagName || attrs != "") ret += ` <${this.tagName + attrs}>`;
-
     if(this.filename) ret += ` filename="${this.filename}"`;
-    if(numChildren > 0) {
-      ret += `{...${numChildren} children...}</${this.tagName}>`;
-    }
+    if(numChildren > 0) ret += `{...${numChildren} children...}</${this.tagName}>`;
     return ret;
   }
   inspect() {
