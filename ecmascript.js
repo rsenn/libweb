8a3c51ff7d51ac7a18f46ca4da9900e857894dec
Roman <roman.l.senn@gmail.com>
2020-05-09 08:11:55 +0200

    ...

---
+++ ecmascript.js
@@ -0,0 +1,6 @@
+//export {ArrayBinding, ArrayLiteral, ArrowFunction, AssignmentExpression, AwaitExpression, BinaryExpression, BindingPattern, BindingProperty, BlockStatement, BreakStatement, CallExpression, ClassDeclaration, ConditionalExpression, ContinueStatement, Declaration, DecoratorExpression, DoStatement, EmptyStatement, Expression, ExpressionStatement, Factory, ForInStatement, ForStatement, FunctionDeclaration, Identifier, IfStatement, ImportStatement, InExpression, JSXLiteral, Literal, LogicalExpression, MemberExpression, NewExpression, Node, ObjectBinding, ObjectLiteral, Program, RestOfExpression, ReturnStatement, SequenceExpression, Statement, StatementList, ThisExpression, ThrowStatement, TryStatement, UnaryExpression, UpdateExpression, VariableDeclaration, VariableDeclarator, WhileStatement, WithStatement } from "./ecmascript/estree.js";
+export { CTORS, estree } from "./ecmascript/estree.js";
+export { Lexer } from "./ecmascript/lexer.js";
+export { Parser } from "./ecmascript/parser.js";
+export { Printer } from "./ecmascript/printer.js";
+export { Token, tokenTypes } from "./ecmascript/token.js";
