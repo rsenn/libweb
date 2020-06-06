15df50e0a9ace7fa2b7578bc4a082412cff62a3b
Roman <roman.l.senn@gmail.com>
2020-06-06 20:54:21 +0200

    ...

--- eagle/document.js
+++ eagle/document.js
@@ -128,8 +128,8 @@ export class EagleDocument extends EagleNode {
       bb.update(bbrect);
     }
 
-    for(let [name,signal] of this.signals) {
-      console.log("signal:",signal);
+    for(let [name, signal] of this.signals) {
+      console.log('signal:', signal);
       let bbrect = signal.getBounds();
 
       bb.update(bbrect);
