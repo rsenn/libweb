b81dc08b9560dfe3b7448f37cf7d6f1d9240ad86
Roman <roman.l.senn@gmail.com>
2020-06-11 06:52:25 +0200

    ...

--- util.js
+++ util.js
@@ -1726,11 +1726,11 @@ Util.equals = function(a, b) {
     return a.length == b.length && a.every((e, i) => b[i] === e);
   }
 };
-
+/*
 Util.isObject = function(obj) {
   const type = typeof obj;
   return type === 'function' || (type === 'object' && !!obj);
-};
+};*/
 
 Util.isGetter = (obj, propName) => {
   while(obj) {
@@ -2322,102 +2322,7 @@ Util.proxyTree = function proxyTree(...callbacks) {
 
   return node([]);
 };
-Util.proxyClone = obj => {
-  const override = Object.create(null);
-  const deleted = Object.create(null);
 
-  const debug = (...args) => console.log('DEBUG proxyClone', ...args); //Util.debug("proxy-clone");
-
-  const get = name => {
-    let value;
-    if(!deleted[name]) value = override[name] || obj[name];
-    if(Util.isObject(value)) {
-      value = Util.proxyClone(value);
-      override[name] = value;
-    }
-    if(typeof value === 'function') {
-      value = value.bind(obj);
-    }
-    return value;
-  };
-
-  return new Proxy(Object.prototype, {
-    getPrototypeOf: () => Object.getPrototypeOf(obj),
-    setPrototypeOf: () => {
-      throw new Error('Not yet implemented: setPrototypeOf');
-    },
-    isExtensible: () => {
-      throw new Error('Not yet implemented: isExtensible');
-    },
-    preventExtensions: () => {
-      throw new Error('Not yet implemented: preventExtensions');
-    },
-    getOwnPropertyDescriptor: (target, name) => {
-      let desc;
-      if(!deleted[name]) {
-        desc = Object.getOwnPropertyDescriptor(override, name) || Object.getOwnPropertyDescriptor(obj, name);
-      }
-      if(desc) desc.configurable = true;
-      debug(`getOwnPropertyDescriptor ${name} =`, desc);
-      return desc;
-    },
-    defineProperty: () => {
-      throw new Error('Not yet implemented: defineProperty');
-    },
-    has: (_, name) => {
-      const has = !deleted[name] && (name in override || name in obj);
-      debug(`has ${name} = ${has}`);
-      return has;
-    },
-    get: (receiver, name) => {
-      const value = get(name);
-      debug(`get ${name} =`, value);
-      return value;
-    },
-    set: (_, name, val) => {
-      delete deleted[name];
-      override[name] = val;
-      debug(`set ${name} = ${val}`, name, val);
-      return true;
-    },
-    deleteProperty: (_, name) => {
-      debug(`deleteProperty ${name}`);
-      deleted[name] = true;
-      delete override[name];
-    },
-    ownKeys: () => {
-      const keys = Object.keys(obj)
-        .concat(Object.keys(override))
-        .filter(Util.uniquePred)
-        .filter(key => !deleted[key]);
-      debug(`ownKeys`, keys);
-      return keys;
-    },
-    apply: () => {
-      throw new Error('Not yet implemented: apply');
-    },
-    construct: () => {
-      throw new Error('Not yet implemented: construct');
-    },
-    enumerate: () => {
-      throw new Error('Not yet implemented: enumerate');
-    }
-  });
-};
-Util.proxyDelegate = (target, origin) => {
-  return new Proxy(target, {
-    get(target, key, receiver) {
-      if(key in target) return Reflect.get(target, key, receiver);
-      const value = origin[key];
-      return typeof value === 'function' ? (...args) => value.apply(origin, args) : value;
-    },
-    set(target, key, value, receiver) {
-      if(key in target) return Reflect.set(target, key, value, receiver);
-      origin[key] = value;
-      return true;
-    }
-  });
-};
 Util.immutable = args => {
   const argsType = typeof args === 'object' && Util.isArray(args) ? 'array' : 'object';
   const errorText = argsType === 'array' ? "Error! You can't change elements of this array" : "Error! You can't change properties of this object";
@@ -2666,8 +2571,8 @@ Object.assign(Util.is, {
 
 Util.assignGlobal = () => Util.weakAssign(Util.getGlobalObject(), Util);
 
-Util.proxyObject = (target, handler) => {
-  const ptr = path => path.reduce((a, i) => a[i], target);
+Util.proxyObject = (root, handler) => {
+  const ptr = path => path.reduce((a, i) => a[i], root);
   const nodes = new WeakMap();
 
   function node(path) {
