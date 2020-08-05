c35aabe1be9aae8006412b3fe1744afb0da0840e
Roman <roman.l.senn@gmail.com>
2020-08-05 09:23:16 +0200

    ...

--- ecmascript.js
+++ ecmascript.js
@@ -4,10 +4,13 @@ export { Parser, ECMAScriptParser } from './ecmascript/parser.js';
 export { Printer } from './ecmascript/printer.js';
 export { Token } from './ecmascript/token.js';
 export { ECMAScriptInterpreter } from './ecmascript/interpreter.js';
+export { Environment } from './ecmascript/environment.js';
 
-import { CTORS, estree, Factory, ESNode } from './ecmascript/estree.js';
+/*import { CTORS, estree, Factory, ESNode } from './ecmascript/estree.js';
 import { Lexer, Stack, PathReplacer } from './ecmascript/lexer.js';
 import { Parser, ECMAScriptParser } from './ecmascript/parser.js';
 import { Printer } from './ecmascript/printer.js';
 import { ECMAScriptInterpreter } from './ecmascript/interpreter.js';
 import { Token } from './ecmascript/token.js';
+import { Environment } from './ecmascript/environment.js';
+*/
