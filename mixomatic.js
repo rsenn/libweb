7865617f6fd7786e7707e4a6127f0e6a9d620b9b
Roman <roman.l.senn@gmail.com>
2021-01-04 07:26:40 +0100

    mixomatic

---
+++ mixomatic.js
@@ -0,0 +1,21 @@
+export default function createMixin(propertyDescriptors) {
+  const mixed = new WeakSet();
+
+  function mixin(obj) {
+    Object.defineProperties(obj, propertyDescriptors);
+    mixed.add(obj);
+    return obj;
+  }
+
+  function checkInstance(obj) {
+    for(let o = obj; o; o = Object.getPrototypeOf(o)) {
+      if(mixed.has(o)) {
+        return true;
+      }
+    }
+  }
+
+  Object.defineProperty(mixin, Symbol.hasInstance, { value: checkInstance });
+
+  return mixin;
+}
