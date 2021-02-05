3f75230558a517c7e7c80d87dba8ea3c595cc7d0
Roman Senn <roman.l.senn@gmail.com>
2021-02-05 16:24:39 +0100

    ...

---
+++ container/Hash-Multimap.js
@@ -0,0 +1,128 @@
+"use strict";
+
+
+   
+  //Adding remove to arrays
+  if(Array.prototype.remove === undefined) {
+    Array.prototype.remove = function () {
+      var result = [];
+      for (var j = arguments.length - 1; j >= 0; j--) {
+        var index = this.length - 1;
+        while (index >= 0) {
+          if (this[index] !== arguments[j]) {
+            index--;
+          } else {
+            result.push(this.splice(index, 1)[0]);
+          }
+        }
+      }
+      return result;
+    }
+  }
+  
+  if (Array.prototype.removeIndex === undefined){
+    Array.prototype.removeIndex = function (index){
+      return this.splice(index, 1);
+    }
+  }
+  
+  var HashMultimap = function () {
+    this._keys = [];
+    this._values = [];
+    this.length = 0;
+  };
+  
+  HashMultimap.prototype.clear = function(){
+    this._keys = [];
+    this._values = [];
+    this.length = 0;
+  };
+  
+  HashMultimap.prototype.get = function(key){
+    var i = this._keys.indexOf(key);
+    
+    if(i >= 0) {
+      return this._values[i];
+    }
+    
+    this._keys.push(key);
+    var res = [];
+    this._values.push(res);
+    return res;
+  };
+  
+  HashMultimap.prototype.put = function(key){
+    var values = this.get(key);
+    var len = arguments.length;
+    var i;
+    
+    for(i = 1; i < len; i++) {
+      values.push(arguments[i]);
+    }
+  };
+  
+  HashMultimap.prototype.remove = function(key){
+    var values = this.get(key);
+    var items = Array.prototype.slice.call(arguments, 1);
+    values.remove(arguments[i]); 
+  };
+  
+  HashMultimap.prototype.removeAll = function(key){
+    var index = this._keys.indexOf(key);
+    
+    if (index >= 0){
+      this._keys.removeIndex(index);
+      this._values.removeIndex(index);
+    }
+  };
+
+  HashMultimap.prototype.replaceValues  = function(key){
+    //TODO: get the rest of the values from the arguments
+
+  };
+
+  HashMultimap.prototype.keys = function(){
+    
+  };
+  
+  HashMultimap.prototype.contains  = function(){
+    //Returns true if the multimap contains the specified key-value pair.
+  };
+  
+  HashMultimap.prototype.containsKey  = function(){
+    //Returns true if the multimap contains any values for the specified key.
+  };
+  
+  HashMultimap.prototype.containsValue  = function(){
+    //Returns true if the multimap contains the specified value for any key.
+  };
+  
+  
+  
+//  HashMultimap.prototype.isEmpty  = function(){
+//
+//  };
+//  
+//  HashMultimap.prototype.isEmpty  = function(){
+//
+//  };
+//  
+//  HashMultimap.prototype.isEmpty  = function(){
+//
+//  };
+//  
+//  HashMultimap.prototype.isEmpty  = function(){
+//
+//  };
+//  
+//  HashMultimap.prototype.isEmpty  = function(){
+//
+//  };
+//  
+//  HashMultimap.prototype.isEmpty  = function(){
+//
+//  };
+  
+
+  export default HashMultimap;
+  
---
+++ container/multibimap.js
@@ -0,0 +1,125 @@
+'use strict';
+
+class _Map {
+  constructor() {
+    this.map = new Map();
+  }
+
+  addIterable(k, v) {
+    if(!this.map.has(k)) this.map.set(k, new Set());
+
+    const set = this.map.get(k);
+    v.forEach(val => set.add(val));
+  }
+
+  add(k, v) {
+    if(!this.map.has(k)) this.map.set(k, new Set());
+
+    this.map.get(k).add(v);
+  }
+
+  get(k) {
+    return this.map.get(k);
+  }
+
+  has(k, v) {
+    if(arguments.length < 2) return this.map.has(k);
+    else return this.map.has(k) && this.map.get(k).has(v);
+  }
+
+  delete(k, v) {
+    if(arguments.length < 2) this.map.delete(k);
+    else if(this.map.has(k)) {
+      this.map.get(k).delete(v);
+    }
+  }
+}
+
+export default class MultiBiMap {
+  constructor(opts = {}) {
+    this.opts = Object.assign({
+        iterableKey: false,
+        iterableValue: false
+      },
+      opts
+    );
+    this._byKey = new _Map();
+    this._byVal = new _Map();
+  }
+
+  getKey(k) {
+    return this._byKey.has(k) ? [...this._byKey.get(k).values()] : false;
+  }
+
+  getVal(v) {
+    return this._byVal.has(v) ? [...this._byVal.get(v).values()] : false;
+  }
+
+  add(k, v, opts = {}) {
+    opts = Object.assign({
+        iterableKey: this.opts.iterableKey,
+        iterableValue: this.opts.iterableValue
+      },
+      opts
+    );
+
+    k = opts.iterableKey && !Array.isArray(k) ? [k] : k;
+    v = opts.iterableValue && !Array.isArray(v) ? [v] : v;
+
+    if(opts.iterableKey && opts.iterableValue) {
+      k.forEach(k => this._byKey.addIterable(k, v));
+      v.forEach(v => this._byVal.addIterable(v, k));
+    } else if(opts.iterableKey) {
+      this._byVal.addIterable(v, k);
+      k.forEach(k => this._byKey.add(k, v));
+    } else if(opts.iterableValue) {
+      this._byKey.addIterable(k, v);
+      v.forEach(v => this._byVal.add(v, k));
+    } else {
+      this._byKey.add(k, v);
+      this._byVal.add(v, k);
+    }
+  }
+
+  has(k, v) {
+    return this._byKey.has(k) && this._byKey.get(k).has(v);
+  }
+
+  hasKey(k) {
+    return this._byKey.has(k);
+  }
+
+  hasVal(v) {
+    return this._byVal.has(v);
+  }
+
+  delete(k, v) {
+    deleteOnly(k, v, this._byKey, this._byVal);
+    deleteOnly(v, k, this._byVal, this._byKey);
+  }
+
+  deleteKey(k) {
+    deleteAll(k, this._byKey, this._byVal);
+  }
+
+  deleteVal(v) {
+    deleteAll(v, this._byVal, this._byKey);
+  }
+}
+
+function deleteAll(a, aMap, bMap) {
+  if(aMap.has(a)) {
+    aMap.get(a).forEach(b => deleteOnly(b, a, bMap));
+    aMap.delete(a);
+  }
+}
+
+function deleteOnly(a, b, aMap) {
+  if(aMap.has(a)) {
+    const bSet = aMap.get(a);
+    bSet.delete(b);
+    if(!bSet.size) {
+      aMap.delete(a);
+    }
+  }
+}
