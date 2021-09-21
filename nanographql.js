0888763647696ffec8c88c85aa285e59a11ef3a3
Roman Senn <roman.l.senn@gmail.com>
2021-09-22 01:49:24 +0200

    ...

---
+++ nanographql.js
@@ -0,0 +1,17 @@
+var getOpname = /(query|mutation) ?([\w\d-_]+)? ?\(.*?\)? \{/;
+
+export function nanographql(str) {
+  str = Array.isArray(str) ? str.join('') : str;
+  var name = getOpname.exec(str);
+  return function(variables) {
+    var data = { query: str };
+    if(variables) data.variables = JSON.stringify(variables);
+    if(name && name.length) {
+      var operationName = name[2];
+      if(operationName) data.operationName = name[2];
+    }
+    return JSON.stringify(data);
+  };
+}
+
+export default nanographql;
