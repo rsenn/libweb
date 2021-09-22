689119d783af119324deeee9f6295b4a1ca93c81
Roman Senn <roman.l.senn@gmail.com>
2021-09-22 02:09:48 +0200

    ...

--- nanographql.js
+++ nanographql.js
@@ -1,11 +1,11 @@
-var getOpname = /(query|mutation) ?([\w\d-_]+)? ?\(.*?\)? \{/;
+var getOpname = /(query|mutation)\s+([^\s{]*)\s*{/;
 
 export function nanographql(str) {
   str = Array.isArray(str) ? str.join('') : str;
   var name = getOpname.exec(str);
   return function(variables) {
     var data = { query: str };
-    if(variables) data.variables = JSON.stringify(variables);
+    data.variables = variables ? JSON.stringify(variables) : null;
     if(name && name.length) {
       var operationName = name[2];
       if(operationName) data.operationName = name[2];
