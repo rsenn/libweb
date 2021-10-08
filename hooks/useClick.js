16d8f650a8ca6d13c381ffea458f6f2a94f824c8
Roman Senn <roman.l.senn@gmail.com>
2021-10-08 16:03:55 +0200

    ...

---
+++ hooks/useClick.js
@@ -0,0 +1,17 @@
+import { Component, useEffect, useRef } from '../dom/preactComponent.js';
+
+export const useClick = onClick => {
+  if(typeof onClick !== 'function') return;
+
+  const element = useRef();
+  useEffect(() => {
+    if(element.current) element.current.addEventListener('click', onClick);
+
+    return () => {
+      if(element.current) element.current.removeEventListener('click', onClick);
+    };
+  }, []);
+  return element;
+};
+
+export default useClick;
---
+++ hooks/useDrag.js
@@ -0,0 +1,62 @@
+import React, { useState } from '../dom/preactComponent.js';
+
+export function useDrag() {
+  // const state = {
+  //   left: 0,
+  //   top: 0
+  // }
+  // const [a, b] = [{left:0, top: 0}, () => {}]
+  const [state, setState] = useState({
+    left: 0,
+    top: 0
+  });
+
+  var handleDown = e => {
+    // var startX = e.clientX;
+    // var startY = e.clientY;
+    // let obj = e.target.getBoundingClientRect();
+    // Reset the starting point
+    document.addEventListener('mousemove', handleMove);
+    document.addEventListener('mouseup', handleUp);
+  };
+
+  var handleMove = e => {
+    // var newX = e.clientX;
+    // var newY = e.clientY;
+    // const diffX = newX;
+    // const diffY = newY;
+    setState({
+      left: e.clientX,
+      top: e.clientY
+    });
+  };
+
+  var handleUp = () => {
+    document.removeEventListener('mousemove', handleMove);
+    document.removeEventListener('mouseup', handleUp);
+  }; // parseUrl
+
+  return {
+    left: state.left,
+    top: state.top,
+    handleDown
+  };
+}
+
+function Drag() {
+  const { left, top, handleDown } = useDrag();
+  return preact.h(
+    'div',
+    {
+      style: {
+        left,
+        top
+      },
+      className: 'dragable',
+      onMouseDown: handleDown
+    },
+    'drag1'
+  );
+}
+
+export default useDrag;
