c2c9f206466b841d47cc61e9e579bdd16db2ded2
Roman Senn <roman.l.senn@gmail.com>
2022-02-13 23:29:09 +0100

    ...

--- pointer.js
+++ pointer.js
@@ -10,7 +10,15 @@ export function DereferenceError(object, member, pos, prev, locator) {
       if(method) method = (frame.typeName || Util.className(frame.thisObj)) + '.' + method;
       else method = frame.getFunctionName();
 
-      return ('' + frame.getFileName()).replace(new RegExp('.*plot-cv/'), '') + ':' + frame.getLineNumber() + ':' + frame.getColumnNumber() + ' ' + method;
+      return (
+        ('' + frame.getFileName()).replace(new RegExp('.*plot-cv/'), '') +
+        ':' +
+        frame.getLineNumber() +
+        ':' +
+        frame.getColumnNumber() +
+        ' ' +
+        method
+      );
     });
   //console.log('member:', member);
   return Object.assign(
@@ -18,7 +26,12 @@ export function DereferenceError(object, member, pos, prev, locator) {
     { object, member, pos, locator },
     {
       message:
-        `Error dereferencing ${Util.className(object)} @ ${Pointer.prototype.toString.call(locator, '/', Pointer.partToString, 'children')}
+        `Error dereferencing ${Util.className(object)} @ ${Pointer.prototype.toString.call(
+          locator,
+          '/',
+          Pointer.partToString,
+          'children'
+        )}
 xml: ${Util.abbreviate(toXML(locator.root || object))}
 no member '${Util.inspect(member, { colors: false })}' in ${Util.inspect(prev, {
           depth: 2,
