cc8e3e32d60fc4f3a4a3e19f01ebf37ee25e827c
Roman Senn <roman.l.senn@gmail.com>
2022-02-20 18:08:02 +0100

    ...

--- color.js
+++ color.js
@@ -1,42 +1,4 @@
 export { HSLA, isHSLA, ImmutableHSLA } from './color/hsla.js';
 export { RGBA, isRGBA, ImmutableRGBA } from './color/rgba.js';
 export { ColoredText } from './color/coloredText.js';
-export {
-  rgb2hue,
-  hue2rgb,
-  luminance2contrast,
-  rgb2whiteness,
-  precision,
-  atan2d,
-  deg2rad,
-  rad2deg,
-  abs,
-  atan2,
-  cos,
-  exp,
-  floor,
-  min,
-  pow,
-  sin,
-  sqrt,
-  epsilon,
-  rgb2hsl,
-  hsl2rgb,
-  rgb2hwb,
-  hwb2rgb,
-  rgb2hsv,
-  hsv2rgb,
-  rgb2xyz,
-  xyz2rgb,
-  hsl2hsv,
-  hsv2hsl,
-  hwb2hsv,
-  hsv2hwb,
-  lab2xyz,
-  xyz2lab,
-  lab2lch,
-  lch2lab,
-  rgb2luminance,
-  rgb2hex,
-  hex2ciede
-} from './color/convert.js';
+export { rgb2hue, hue2rgb, luminance2contrast, rgb2whiteness, precision, atan2d, deg2rad, rad2deg, abs, atan2, cos, exp, floor, min, pow, sin, sqrt, epsilon, rgb2hsl, hsl2rgb, rgb2hwb, hwb2rgb, rgb2hsv, hsv2rgb, rgb2xyz, xyz2rgb, hsl2hsv, hsv2hsl, hwb2hsv, hsv2hwb, lab2xyz, xyz2lab, lab2lch, lch2lab, rgb2luminance, rgb2hex, hex2ciede } from './color/convert.js';
