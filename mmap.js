0e5e01a375494b2a6946f211c2145c2f9172d0b7
Roman Senn <roman.l.senn@gmail.com>
2022-08-21 22:33:03 +0200

    ...

---
+++ mmap.js
@@ -0,0 +1,41 @@
+export const EBUSY = 16;
+export const EFAULT = 14;
+export const EINVAL = 22;
+export const ENOMEM = 12;
+export const MAP_ANONYMOUS = 32;
+export const MAP_DENYWRITE = 2048;
+export const MAP_EXECUTABLE = 4096;
+export const MAP_FAILED = -1;
+export const MAP_FIXED = 16;
+export const MAP_GROWSDOWN = 256;
+export const MAP_HUGETLB = 262144;
+export const MAP_LOCKED = 8192;
+export const MAP_NONBLOCK = 65536;
+export const MAP_NORESERVE = 16384;
+export const MAP_POPULATE = 32768;
+export const MAP_PRIVATE = 2;
+export const MAP_SHARED = 1;
+export const MAP_STACK = 131072;
+export const MAP_TYPE = 15;
+export const MS_ASYNC = 1;
+export const MS_INVALIDATE = 2;
+export const MS_SYNC = 4;
+export const PROT_EXEC = 4;
+export const PROT_GROWSDOWN = 16777216;
+export const PROT_GROWSUP = 33554432;
+export const PROT_NONE = 0;
+export const PROT_READ = 1;
+export const PROT_SEM = 8;
+export const PROT_WRITE = 2;
+
+export function filename() {}
+    
+export function mmap() {}
+    
+export function mprotect() {}
+    
+export function msync() {}
+    
+export function munmap() {}
+    
+export function toString() {}
