e3beef6701f1c8913bd0e8e8596c59490f86b456
Roman Senn <roman.l.senn@gmail.com>
2025-10-26 11:47:54 +0100

    ...

--- svg/path-utils.js
+++ svg/path-utils.js
@@ -2,7 +2,7 @@ import { parse, parseSVG, makeAbsolute } from './path-parser.js';
 import { SvgPath, Command } from './path.js';
 import { Matrix } from '../geom/matrix.js';
 import { Point } from '../geom.js';
-import { isObject, partitionArray } from '../misc.js';
+import { isObject, partitionArray, define, nonenumerable } from '../misc.js';
 
 export const isUpper = s => s.toUpperCase() == s;
 
@@ -37,162 +37,165 @@ export function parseSvgPath(str, absolute) {
 
 parseSvgPath.prototype = new SvgPath();
 
-Object.assign(parseSvgPath.prototype, {
-  str(delim = '\n') {
-    return this.subpaths()
-      .map(sp => SvgPath.prototype.str.call({ commands: sp }))
-      .join(delim);
+define(
+  parseSvgPath.prototype,
+  nonenumerable({
+    str(delim = '\n') {
+      return this.subpaths()
+        .map(sp => SvgPath.prototype.str.call({ commands: sp }))
+        .join(delim);
 
-    // return this.commands.reduce((s, c) => (s == '' ? '' : s + delim) + c.toString(), '');
-  },
-  toRelative() {
-    const commands = [];
-    let pos = [0, 0];
+      // return this.commands.reduce((s, c) => (s == '' ? '' : s + delim) + c.toString(), '');
+    },
+    toRelative() {
+      const commands = [];
+      let pos = [0, 0];
 
-    for(let c of this.commands) {
-      let { name, args } = c;
+      for(let c of this.commands) {
+        let { name, args } = c;
 
-      if(/[Mm]/.test(name)) {
-        let [x, y] = args.slice(-2);
+        if(/[Mm]/.test(name)) {
+          let [x, y] = args.slice(-2);
 
-        if(!pos || isUpper(name)) {
-          let dx = x - pos[0],
-            dy = y - pos[1];
-          pos = [x, y];
-          args = [dx, dy];
-        } else {
-          pos[0] += x;
-          pos[1] += y;
+          if(!pos || isUpper(name)) {
+            let dx = x - pos[0],
+              dy = y - pos[1];
+            pos = [x, y];
+            args = [dx, dy];
+          } else {
+            pos[0] += x;
+            pos[1] += y;
+          }
+          name = 'm';
+        } else if(/[Zz]/.test(name)) {
+          name = 'z';
+        } else if(isUpper(name)) {
         }
-        name = 'm';
-      } else if(/[Zz]/.test(name)) {
-        name = 'z';
-      } else if(isUpper(name)) {
+
+        commands.push(Object.setPrototypeOf({ name, args }, SvgCommand.prototype));
       }
 
-      commands.push(Object.setPrototypeOf({ name, args }, SvgCommand.prototype));
-    }
+      return Object.setPrototypeOf({ commands }, parseSvgPath.prototype);
+    },
+    subpaths() {
+      let p,
+        result = [(p = [])];
 
-    return Object.setPrototypeOf({ commands }, parseSvgPath.prototype);
-  },
-  subpaths() {
-    let p,
-      result = [(p = [])];
+      for(let c of this.commands) {
+        p.push(c);
 
-    for(let c of this.commands) {
-      p.push(c);
-
-      if(/[Zz]/.test(c.name)) result.push((p = []));
-    }
-
-    return result;
-  },
-  transform(matrix) {
-    const commands = [];
-
-    if(!isObject(matrix) || !(matrix instanceof Matrix)) matrix = new Matrix(matrix);
-
-    for(let c of this.commands) {
-      const { name, args } = c;
-
-      if((commands.length == 0 || isUpper(name)) && args.length >= 2) {
-        let i = args.length - 2;
-        let [x, y] = args.splice(i, 2);
-
-        [x, y] = matrix.transformXY(x, y);
-
-        args.push(x, y);
-      } else if(/[HhVv]/.test(name)) {
-        let i = 'hv'.indexOf(name.toLowerCase());
-        let [arg] = args;
-
-        args[0] = matrix.transformWH(arg, arg)[i];
-      } else if(!isUpper(name) && args.length >= 2) {
-        args.splice(0, args.length, ...[...partitionArray(args, 2)].map(([x, y]) => matrix.transformWH(x, y)));
+        if(/[Zz]/.test(c.name)) result.push((p = []));
       }
 
-      commands.push(Object.setPrototypeOf({ name, args }, SvgCommand.prototype));
-    }
+      return result;
+    },
+    transform(matrix) {
+      const commands = [];
 
-    return Object.setPrototypeOf({ commands }, parseSvgPath.prototype);
-  },
-  *points() {
-    let x = 0,
-      y = 0;
-    let originX = 0,
-      originY = 0;
+      if(!isObject(matrix) || !(matrix instanceof Matrix)) matrix = new Matrix(matrix);
 
-    for(let { name, args } of this.commands) {
-      let [dx, dy] = (args ?? []).slice(-2);
+      for(let c of this.commands) {
+        const { name, args } = c;
 
-      switch (name) {
-        case 'm':
-        case 'M': {
-          break;
+        if((commands.length == 0 || isUpper(name)) && args.length >= 2) {
+          let i = args.length - 2;
+          let [x, y] = args.splice(i, 2);
+
+          [x, y] = matrix.transformXY(x, y);
+
+          args.push(x, y);
+        } else if(/[HhVv]/.test(name)) {
+          let i = 'hv'.indexOf(name.toLowerCase());
+          let [arg] = args;
+
+          args[0] = matrix.transformWH(arg, arg)[i];
+        } else if(!isUpper(name) && args.length >= 2) {
+          args.splice(0, args.length, ...[...partitionArray(args, 2)].map(([x, y]) => matrix.transformWH(x, y)));
         }
-        case 'z':
-        case 'Z': {
-          dx = originX;
-          dy = originY;
-          name = 'Z';
-          break;
-        }
-        case 'l':
-        case 'L': {
-          break;
-        }
-        case 'h':
-        case 'H': {
-          dy = 0;
-          break;
-        }
-        case 'v':
-        case 'V': {
-          dy = dx;
-          dx = 0;
-          break;
-        }
-        case 'c':
-        case 'C': {
-          break;
-        }
-        case 's':
-        case 'S': {
-          break;
-        }
-        case 'q':
-        case 'Q': {
-          break;
-        }
-        case 't':
-        case 'T': {
-          break;
-        }
-        case 'a':
-        case 'A': {
-          break;
-        }
+
+        commands.push(Object.setPrototypeOf({ name, args }, SvgCommand.prototype));
       }
 
-      if(isUpper(name)) {
-        dx -= x;
-        dy -= y;
-      }
+      return Object.setPrototypeOf({ commands }, parseSvgPath.prototype);
+    },
+    *points() {
+      let x = 0,
+        y = 0;
+      let originX = 0,
+        originY = 0;
 
-      /*if(dx != 0 || dy != 0)*/ {
-        x += dx;
-        y += dy;
+      for(let { name, args } of this.commands) {
+        let [dx, dy] = (args ?? []).slice(-2);
 
-        yield new Point(x, y);
-      }
+        switch (name) {
+          case 'm':
+          case 'M': {
+            break;
+          }
+          case 'z':
+          case 'Z': {
+            dx = originX;
+            dy = originY;
+            name = 'Z';
+            break;
+          }
+          case 'l':
+          case 'L': {
+            break;
+          }
+          case 'h':
+          case 'H': {
+            dy = 0;
+            break;
+          }
+          case 'v':
+          case 'V': {
+            dy = dx;
+            dx = 0;
+            break;
+          }
+          case 'c':
+          case 'C': {
+            break;
+          }
+          case 's':
+          case 'S': {
+            break;
+          }
+          case 'q':
+          case 'Q': {
+            break;
+          }
+          case 't':
+          case 'T': {
+            break;
+          }
+          case 'a':
+          case 'A': {
+            break;
+          }
+        }
 
-      if(/[Mm]/.test(name)) {
-        originX = x;
-        originY = y;
+        if(isUpper(name)) {
+          dx -= x;
+          dy -= y;
+        }
+
+        /*if(dx != 0 || dy != 0)*/ {
+          x += dx;
+          y += dy;
+
+          yield new Point(x, y);
+        }
+
+        if(/[Mm]/.test(name)) {
+          originX = x;
+          originY = y;
+        }
       }
-    }
-  },
-});
+    },
+  }),
+);
 
 export function MakeText(str, font) {
   font ??= JSON.parse(fs.readFileSync('font.json', 'utf-8'));
