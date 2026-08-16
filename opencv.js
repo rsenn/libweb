c91d55167e3844534ba31346d1bdf4f70b444389
Roman Senn <roman.l.senn@gmail.com>
2026-08-16 02:26:23 +0200

    ...

--- opencv.js
+++ opencv.js
@@ -1,13 +1,1 @@
-import { TickMeter } from 'utility.so';
-import { Contour } from 'contour.so';
-import * as cv from 'cv.so';
-import * as draw from 'draw.so';
-import { Line } from 'line.so';
-import { Mat } from 'mat.so';
-import { PointIterator } from 'point-iterator.so';
-import { Point } from 'point.so';
-import { Rect } from 'rect.so';
-import { Size } from 'size.so';
-import { VideoCapture } from 'video-capture.so';
-
-export { cv, draw, Contour, Line, Mat, Point, PointIterator, Rect, Size, TickMeter, VideoCapture };
+opencv-5.0.js
